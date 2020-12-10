4b571c00fc374de93663ed851f1e5d37e1aa50d9
veljajenajbolji <veljaghetto@gmail.com>
2020-12-10 19:55:04 +0100

    nearly done website

---
+++ assets/js/custom.js
@@ -0,0 +1,96 @@
+(function ($) {
+
+	"use strict";
+
+	//Prikazuje pozadinu headera ako je skrolovano vise od  visine headera
+	$(window).scroll(function () {
+		var scroll = $(window).scrollTop();
+		var box = $('.header-text').height();
+		var header = $('header').height();
+
+		if (scroll >= box - header) {
+			$("header").addClass("background-header");
+		} else {
+			$("header").removeClass("background-header");
+		}
+	});
+
+	// Window Resize Mobile Menu Fix
+	mobilnaNavigacija();
+
+
+	// Animacije kada element udje unutar viewport-a
+	window.sr = new scrollReveal();
+
+		var menuOpen = false
+		//Otvaranje i zatvaranje mobile-menija
+		$(".menu-trigger").on('click', function () {
+			$('.menu-trigger').toggleClass('active');
+			$('.header-area .nav').fadeToggle(100);
+			menuOpen = !menuOpen;
+			console.log(menuOpen)
+		});
+
+
+	$(document).ready(function () {
+		$('#welcome').addClass('active');
+
+		//Klik zatvara meni na mobilnom
+		$('.menu-item').on('click', function (e) {
+			if(menuOpen){
+				$('.menu-trigger').toggleClass('active');
+				$('.header-area .nav').slideToggle(300);
+				menuOpen = !menuOpen;
+
+			}
+		});
+	});
+
+	//Podesiti 'active' klasu u navigaciji na ono sto je u viewportu
+	$(window).scroll(function (event) {
+		var scrollPos = $(document).scrollTop() + 80;
+
+		if (scrollPos === 0) {
+			$('a[href^="#welcome"]').addClass('active');
+			return;
+		}
+		$('.menu-item').not('[href=""]').not('[href="javascript:;"]').each(function () {
+			var currLink = $(this);
+			var refElement = $(currLink.attr("href"));
+
+			if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
+				$('.menu-item').removeClass("active");
+				currLink.addClass("active");
+			} else {
+				currLink.removeClass("active");
+			}
+		});
+	})
+	// Kada se promeni velicina browsera poziva se mobilnaNavigacija()
+	$(window).on('resize', function () {
+		mobilnaNavigacija();
+	});
+
+	function mobilnaNavigacija() {
+		var width = $(window).width();
+		$('.submenu').on('click', function () {
+			if (width < 992) {
+				$('.submenu ul').removeClass('active');
+				$(this).find('ul').toggleClass('active');
+			}
+		});
+	}
+	//rotiranje telefona sa leve strane u odnosu na poziciju misa unutar sectiona
+		$("#promotion").on('mousemove',(function(e) {
+
+		  var offset = $(this).offset();
+		  var relativeY = (e.pageY - offset.top);
+		  $('#leftPhoneImg').css('transition', '0.2s')
+		  $('#leftPhoneImg').css('-webkit-transform','rotate('+relativeY/30+'deg)'); 
+		  $('#leftPhoneImg').css('-moz-transform','rotate('+relativeY/30+'deg)');
+		  $('#leftPhoneImg').css('transform','rotate('+relativeY/30+'deg)');		
+		}));
+	
+
+})(window.jQuery);
+
---
+++ assets/js/dinamickiSadrzaj.js
@@ -0,0 +1,117 @@
+window.addEventListener('load', function(){
+	//dinamicko ispisivanje kartica
+	var karticeSadrzaj = [
+		['01', 'features-icon-1.png', 'Analiziramo Trendove', 'Vršimo analizu i detaljno proučavamo tržište kao i sve probleme koje možemo da rešimo za vas', '#testimonials', 'enter left move 30px over 0.6s after 0.4s', 'Modal1','https://www.youtube.com/embed/VLoa4XBFWhg'],
+		['02', 'features-icon-2.png', 'Optimizujemo Rešenja', 'Pružamo potpunu optimizaciju vaše aplikacije i staramo se o tome da to tako i ostane. Zauvek.', '#testimonials', 'enter bottom move 30px over 0.6s after 0.4s', 'Modal2','https://www.youtube.com/embed/QF5eWapHM5s'],
+		['03', 'fire.png', 'Pravimo Proizvod', 'Pažljivo i brzo dolazimo do rešenja koje vas sigurno neće izneveriti. Naši korisnici vam mogu potvrditi isto.', '#testimonials', 'enter right move 30px over 0.6s after 0.4s', 'Modal3','https://www.youtube.com/embed/dgFlgAxwwoM']
+
+	];
+	var cardHolder = document.getElementById('cardHolder');
+	for (var i = 0; i < karticeSadrzaj.length; i++){
+		cardHolder.innerHTML += `
+		
+				<div class="col-lg-4 col-md-6 col-sm-12 col-xs-12"
+                    data-scroll-reveal="`+karticeSadrzaj[i][5] +`">
+                    <div class="features-item">
+                        <div class="features-icon">
+                            <h2>`+ karticeSadrzaj[i][0]+`</h2>
+                            <img src="assets/images/`+ karticeSadrzaj[i][1]+`" alt="">
+                            <h4>`+ karticeSadrzaj[i][2]+`</h4>
+                            <p>`+ karticeSadrzaj[i][3]+`</p>
+                            <button type="button" class="main-button" data-toggle="modal" data-target="#`+ karticeSadrzaj[i][6] +`">
+                                Read More
+                            </button>
+                        </div>
+                    </div>
+                </div>
+		`
+    }
+    var modalHolder = document.getElementById('modalHolder');
+	for (var i = 0; i < karticeSadrzaj.length; i++){
+		modalHolder.innerHTML += `
+
+		<div class="modal fade" id="`+ karticeSadrzaj[i][6] +`" tabindex="-1" role="dialog" aria-labelledby="`+ karticeSadrzaj[i][6] +`Label" aria-hidden="true">
+		<div class="modal-dialog modal-dialog-centered" role="document">
+			<div class="modal-content">
+			<div class="modal-header">
+				<h5 class="modal-title" id="`+ karticeSadrzaj[i][6] +`Label">`+ karticeSadrzaj[i][2] +`</h5>
+				<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+				<span aria-hidden="true">&times;</span>
+				</button>
+			</div>
+			<div class="modal-body">
+				<iframe width="100%" height="300" src="`+ karticeSadrzaj[i][7] +`" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+				
+				</iframe>
+			</div>
+			</div>
+		</div>
+		</div>
+		`
+	}
+
+
+	//dinamicko ucitavanje specifikacija
+	var specifikacijeSadrzaj = [
+		['about-icon-01.png','Naše aplikacije su brze','Koristimo najnovije tehnologije i staramo se da svaki aspekt našeg proizvoda radi kao švajcarski sat!','enter right move 30px over 0.6s after 0.4s'],
+		['about-icon-02.png','Naše Aplikacije su lepe','Upotrebom najsavremenijeg dizajna i učešćem našeg UX/UI tima dobijate maksimalnu estetičnost dizajna.','enter right move 30px over 0.6s after 0.5s'],
+		['about-icon-03.png','Naše aplikacije rade','Proizvodi, koje pravi Vector, su pouzdani i retko se kvare. Konstantna podrška i održavanje se kod nas podrazumevaju.','enter right move 30px over 0.6s after 0.6s']
+	];
+
+	var specifikacijeHolder = document.getElementById('specifikacijeHolder');
+	for (var i = 0; i < specifikacijeSadrzaj.length; i++){
+
+		specifikacijeHolder.innerHTML += `
+		
+		<li data-scroll-reveal="`+specifikacijeSadrzaj[i][3] +`">
+			<img src="assets/images/`+specifikacijeSadrzaj[i][0] +`" alt="">
+				<div class="text">
+					<h4>`+specifikacijeSadrzaj[i][1] +`</h4>
+					<p>`+specifikacijeSadrzaj[i][2] +`</p>
+				</div>
+		</li>
+		`
+	}
+
+    //dinamicko ispisivanje utisaka
+	var utisciSadrzaj = [
+		['testimonial-author-1.png', 5, 'Marko Petrović', 'Moja iskustva sa Vector-om su sjajna! Nisam očekivao da će moja aplikacija biti gotova tako brzo i cela moja firma je zadovoljna našom novom internom aplikacijom. Svima preporučujem saradnju!', 'Generalni Direktor OpenDoor'],
+		['testimonial-author-2.png', 3, 'Ivan Janković', 'Unajmio sam Vector tim da mi naprave aplikaciju kojom bih pratio napredak svoje publike - pokazali su se odlično', 'Influenser'],
+		['testimonial-author-3.png', 2, 'Ana Simović', 'Zaista oduševljena saradnjom! Podrška koja mi je pružena u toku pravljenja moje aplikacije je na drugom nivou! Uvek sam bila obaveštena o svemu što se dešava!', 'Managing Director - Clear Solutions'],
+
+	];
+
+	var utisciHolder = document.getElementById('utisciHolder');
+	for (var i = 0; i < utisciSadrzaj.length; i++){
+
+
+		utisciHolder.innerHTML += `
+
+    <div class="item service-item">
+		<div class="author">
+			<i><img src="assets/images/`+ utisciSadrzaj[i][0] +`" alt="Author Third"></i>
+		</div>
+		<div class="testimonial-content">
+			<h4>`+ utisciSadrzaj[i][2] +`</h4>
+			<p>“`+ utisciSadrzaj[i][3] +`”</p>
+			<span>`+ utisciSadrzaj[i][4] +`</span>
+		</div>
+	</div>
+
+
+		`
+	}
+
+	//dinamicko ispisivanje usluga unutar 'select' taga
+
+	var uslugeSadrzaj = ['Web Dizajn', 'App Development','SEO Optimizacija', 'UX/UI Design'];
+
+	var uslugeHolder = document.getElementById('usluga');
+	for (var i = 0; i < uslugeSadrzaj.length; i++){
+
+
+		uslugeHolder.innerHTML += `
+		<option value="`+ i +`">`+uslugeSadrzaj[i] +`</option>
+		`
+	}
+});
